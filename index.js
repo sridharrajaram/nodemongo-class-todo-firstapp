50ac1345e395fdf408eeddd95473aab40f0e60ef
Sridhar Rajaram <srilumia@hotmail.com>
2021-09-08 16:34:50 +0530

    3001 removed

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ const jwt = require('jsonwebtoken');
 const dotenv = require('dotenv');
 dotenv.config();
 console.log(process.env);
-const PORT = process.env.PORT || 3001;
-//const url = "mongodb://localhost:27017"
+const PORT = process.env.PORT
 const url = process.env.DB_URL
 
 
